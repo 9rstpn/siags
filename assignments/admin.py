55d44c8acc4d60e0f399018c23647cf7498db1e8
Prashanth Nair <54662001+9rstpn@users.noreply.github.com>
2022-09-20 22:09:39 +0530

    Delete assignments directory

--- assignments/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from assignments.models import Assignment, AssignmentSubmission
-
-admin.site.register(Assignment)
-admin.site.register(AssignmentSubmission)
